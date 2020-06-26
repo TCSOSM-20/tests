72556ca8d5a07199c182a586688bbebe8cf1cfad
Felipe Vicens <felipe.vicens@atos.net>
2020-06-26 16:23:34 +0200

    Fix prometheus port for inside stack tests
    Change-Id: Id57a6e90a209fa166605a4ad1e148fb96d88aa3b
    Signed-off-by: Felipe Vicens <felipe.vicens@atos.net>

--- robot-systest/resources/basic_01-crud_operations_on_vim_targets_data.py
+++ robot-systest/resources/basic_01-crud_operations_on_vim_targets_data.py
@@ -17,9 +17,10 @@ from pathlib import Path
 # Prometheus host and port
 if os.environ.get("PROMETHEUS_HOSTNAME", False):
     prometheus_host = os.environ.get("PROMETHEUS_HOSTNAME")
+    prometheus_port = "9090"
 else:
     prometheus_host = os.environ.get("OSM_HOSTNAME")
-prometheus_port = "9091"
+    prometheus_port = "9091"
 
 # VIM Configuration
 vim_account_type = "openstack"
